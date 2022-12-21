45e7c42433c0ca79d28b0c8babddd286367cdb76
EMshady92 <daniel24992@gmail.com>
2022-12-21 10:30:34 -0600

    use_form

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use DB;
+use Form;
 use Illuminate\Support\Facades\Auth;
 use Session;
 use App\User;
