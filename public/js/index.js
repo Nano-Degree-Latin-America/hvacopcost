988e60ac61876a651aa1c5578bc9a8acab38b564
EMshady92 <daniel24992@gmail.com>
2023-03-10 10:38:59 -0600

    hidden_button

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     getPaises();
     traer_categorias_edif();
-    $('#div_next').addClass("hidden");
+    $('#div_next_h').addClass("hidden");
     $('#calcular').attr('disabled', true);
     $('#calcular').css('background-color','gray');
  /*    $('#next').attr('disabled', true); */
