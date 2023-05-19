dfde79eb17f7f83346964df7ec1d1e283ec09c1d
EMshady92 <daniel24992@gmail.com>
2023-05-19 15:33:54 -0600

    step_1

--- resources/views/resultados2.blade.php
+++ resources/views/resultados2.blade.php
@@ -5113,7 +5113,7 @@ function confort_b(val_conf_b){
      javascript:history.forward(1)
 function app() {
 			return {
-				step: 5,
+				step: 1,
 				passwordStrengthText: '',
 				togglePassword: false,
 
