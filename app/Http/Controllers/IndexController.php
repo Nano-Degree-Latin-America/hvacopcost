0ba148455e183dfa97532fea90565059b2ce7601
EMshady92 <daniel24992@gmail.com>
2024-04-30 11:56:09 -0600

    verified

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -18,7 +18,7 @@ class IndexController extends Controller
      */
     public function __construct()
     {
-        $this->middleware(['auth', 'verified']);
+        $this->middleware(['auth']);
     }
 
 
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
     public function __construct()
     {
        /*  $this->middleware('auth'); */
-       $this->middleware(['auth', 'verified']);
+       $this->middleware(['auth']);
     }
 
 
